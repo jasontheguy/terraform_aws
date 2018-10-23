7a4f15731ba9c8d0ee454c6379f09de7df3e9b50
Jason Renaud <renaudjj@g.cofc.edu>
2018-10-23 16:39:07 -0600

    Added in elb security group and health check by load balancer

--- main.tf
+++ main.tf
@@ -41,8 +41,42 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+  listener{
+    lb_port =80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+  health_check{
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+
+
+  }
+
+  resource "aws_security_group" "elb"{
+    name = "terraform-example-elb"
+    ingress{
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  
+data "aws_availability_zones" "all" {}
+
 resource "aws_autoscaling_group" "example"{
-  launch_configuration ="${aws_launch_configuration.example.id}"
+launch_configuration ="${aws_launch_configuration.example.id}"
+availability_zones = ["${data.aws_availability_zones.all.names}"]
 min_size = 2
 max_size = 10
 
@@ -53,6 +87,7 @@ tag{
 }
 
 
+
 }
-//Left off on page 69...lol
+
 
